5a096c86fa9a12a9da3f4e8b3f000463b6698e4b
caffia <francocafferata@hotmail.com>
2015-10-21 18:14:39 -0300

    nuevos cambios en las interfaces. :P

---
+++ interfas/Error_alta_baja.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 21 18:02:42 2015
+
+@author: Franco
+"""
+
+from PyQt4 import QtGui, uic, QtCore
+
+error_dialogo_alta_baja = uic.loadUiType('Error_cliente.ui')[0]
+
+class Error_alta_baja(QtGui.QDialog, error_dialogo_alta_baja):
+    
+    def __init__(self, parent = None):
+        QtGui.QDialog.__init__(self, parent)
+        self.setupUi(self)
